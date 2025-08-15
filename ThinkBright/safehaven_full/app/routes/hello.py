4f7bede53aaf51d71f3cdf6893983560c05b11ae
damondfranklin2-cpu <damon.d.franklin2@gmail.com>
2025-08-15 13:09:09 -0400

    Delete ThinkBright directory

--- ThinkBright/safehaven_full/app/routes/hello.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.models.hello_model import HelloResponse
-
-router = APIRouter()
-
-@router.get("/hello", response_model=HelloResponse)
-def say_hello():
-    return {"message": "Hello, world!"}
